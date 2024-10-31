179a5b873caaa6d27772b221b1a8ab9fc27872c2
Henry Mai <henryzmai@gmail.com>
2024-10-31 17:04:08 -0400

    Update armEncoder.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/armEncoder.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/armEncoder.java
@@ -10,49 +10,86 @@ import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
 @TeleOp(name = "Encoder Test ")
 public class armEncoder extends LinearOpMode {
-//    DcMotor armEncoder;
-    DcMotor pivotMotor;
+    DcMotor armPivot;
+    //DcMotor armEncoder;
 
     //MY VARIABLES
-    double ticks = 8192;
-    double newTarget;
+    int ticks = 8192/2;
+    double newTarget = 2048;
+
+    // Position of the arm when it's lifted
+    int armUpPosition = -1920;
+
+    // Position of the arm when it's down
+    int armDownPosition = 0;
 
     @Override
     public void runOpMode() throws InterruptedException {
-        pivotMotor = hardwareMap.get(DcMotor.class, "armEncoder");
+        armPivot = hardwareMap.dcMotor.get("armEncoder");
+        armPivot.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         telemetry.addData("Hardware: ", "Initialized");
-        pivotMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        pivotMotor = hardwareMap.dcMotor.get("pivotMotor");
-
+        telemetry.update();
         waitForStart();
 
         while (opModeIsActive()) {
-            if(gamepad1.x){
-                encoder(0.125);
+            double CPR = 28 * 5.23 * 5.23 * 2;
+
+            int encoderValue = armPivot.getCurrentPosition();
+            double revolutions = encoderValue/CPR;
+
+            double angle = revolutions * 360;
+            double angleNormalized = angle % 360;
+
+            // If the X button is pressed, raise the arm
+            if (gamepad1.x && ((-1* encoderValue) <= 1920.0)) {
+                if((-1* encoderValue) >= 1820.0){
+                    double differencePOS = 1920.0 - (-1* encoderValue);
+                    double logval = Math.log10(differencePOS);
+                    armPivot.setPower(Math.pow(0.3, logval));
+                    telemetry.addData("logval", logval);
+                } else {
+                    armPivot.setPower(0.6);
+                }
+
             }
-            telemetry.addData("Motor Ticks: ", pivotMotor.getCurrentPosition());
 
-            if(gamepad1.a){ // this sets the pivotMotor back to its original position i think
-                tracker();
+            // If the A button is pressed, lower the arm
+            else if (gamepad1.a && ((-1* encoderValue) >= 4.0))  {
+                armPivot.setPower(-0.6);
+
             }
-    }
+            else {
+                armPivot.setPower(0);
+            }
+
+            // Get the current position of the armPivot
+            double position = armPivot.getCurrentPosition();
+
+            // Get the target position of the armPivot
+            double desiredPosition = armPivot.getTargetPosition();
+
+            // Show the position of the armPivot on telemetry
+            telemetry.addData("Encoder Position", position);
+
+            // Show the target position of the armPivot on telemetry
+            telemetry.addData("Desired Position", desiredPosition);
+
+            //telemetry.update();
+            telemetry.addData("encoder Ticks: ", encoderValue);
+            telemetry.addData("motor revs: ", revolutions);
+            telemetry.addData("arm angle: ", angle);
+
+            telemetry.update();
 
     }
 
-    public void encoder(double turnage){
-        newTarget = ticks/turnage;
-        pivotMotor.setTargetPosition((int)newTarget);
-        pivotMotor.setPower(0.55);
-        pivotMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION); // for some reason it doesnt stop so i added and if statement
-//        if (armEncoder.getCurrentPosition() == newTarget) {
-//            pivotMotor.setPower(0);
-//        }
     }
 
-    public void tracker(){
-        pivotMotor.setTargetPosition(0);
-        pivotMotor.setPower(0.8);
-        pivotMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+/**
+    public void return_position(){
+        armEncoder.setTargetPosition(0);
+        armEncoder.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        armEncoder.setPower(0.1);
     }
-
+**/
 }
