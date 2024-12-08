11107c61c1a5db250eaec80e4d46450a03618979
Henry Mai <henryzmai@gmail.com>
2024-12-08 15:22:45 -0500

    Update MecanumDrive.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -77,16 +77,16 @@ import java.util.List;
             // path profile parameters (in inches)
             public double maxWheelVel = 50;
             public double minProfileAccel = -30;
-            public double maxProfileAccel = 50;
+            public double maxProfileAccel = 25;
 
             // turn profile parameters (in radians)
             public double maxAngVel = Math.PI; // shared with path
             public double maxAngAccel = Math.PI;
 
-            // path controller gains
-            public double axialGain = 0.0;
-            public double lateralGain = 0.0;
-            public double headingGain = 0.0; // shared with turn
+            // path controller gains (on laptop gamepad blue circle is irl and green circle is where it is meant to be) x and y swapped for some reason on telemetry
+            public double axialGain = 0.0; // forward and backwards
+            public double lateralGain = 5.0;
+            public double headingGain = 15.0; // shared with turn; rotational
 
             public double axialVelGain = 0.0;
             public double lateralVelGain = 0.0;
