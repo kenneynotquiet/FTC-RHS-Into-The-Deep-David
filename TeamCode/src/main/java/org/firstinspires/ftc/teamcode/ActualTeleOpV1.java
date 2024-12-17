2257a661e66c25e80048291e7bd487971d1e64ac
IamPenniless <36608@rpsk12.org>
2024-12-17 16:49:10 -0500

    Add files via upload

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ActualTeleOpV1.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.PoseVelocity2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.Drawing;
+import org.firstinspires.ftc.teamcode.MecanumDrive;
+import org.firstinspires.ftc.teamcode.TankDrive;
+
+public class ActualTeleOpV1 {
+}
